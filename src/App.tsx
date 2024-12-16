636e7b6f1415df707044eda5b4c944b89e8bc23e
ervan <ervanari88@gmail.com>
2024-12-16 14:52:13 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FloatingMedia from './components/FloatingWhatsApp';
+
+const agents = [
+    { name: 'Call', phone: '6281234567890', icon: 'https://img.icons8.com/3d-fluency/94/telephone-handset.png' },
+    { name: 'Email', phone: '6289876543210', icon: 'https://img.icons8.com/3d-fluency/94/gmail.png' },
+    { name: 'Email Broadcast', phone: '6281234567890', icon: 'https://img.icons8.com/ios/50/online-support--v1.png' },
+    { name: 'Facebook', phone: '6289876543210', icon: 'https://img.icons8.com/3d-fluency/94/facebook-messenger.png' },
+    { name: 'Instagram', phone: '6281234567890', icon: 'https://img.icons8.com/3d-fluency/94/instagram-new.png' },
+    { name: 'Live Chat', phone: '6289876543210', icon: 'https://img.icons8.com/3d-fluency/94/chat.png' },
+    { name: 'Voice Chat', phone: '6289876543210', icon: 'https://img.icons8.com/3d-fluency/94/chatbot.png' },
+    { name: 'Whatsapp', phone: '6289876543210', icon: 'https://img.icons8.com/3d-fluency/94/whatsapp.png' },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <FloatingMedia
+            agents={agents}
+            message="Hello, I need assistance!"
+            panelWidth="350px"
+            fabColor="primary"
+            fabPosition="bottom-left"
+        />
+      </div>
   );
 }
 
